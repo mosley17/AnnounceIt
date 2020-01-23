b1331e4d042ddcc36ff3e9813b13868339de22ef
mosley17 <maniraguhamosley@gmail.com>
2020-01-23 20:38:34 +0200

    completing some features

--- UI/js/index.js
+++ UI/js/index.js
@@ -5,10 +5,31 @@ document.querySelector("#passwordId").addEventListener("blur", validatePassword)
 
 function validateUsername(){
     const username= document.querySelector("#userId");
-    const regExp= /^[a-zA-Z]{3-10}$/;
+    const errorUser= document.querySelector(".errorUser")
+    const regExp= /^[a-zA-Z]{6,10}$/;
     if(!regExp.test(username.value)){
-        username.classList.add("error")
+        errorUser.style.display="block";
     } else {
-        username.classList.remove('error')
+        errorUser.style.display="none";
+    }
+}
+function validatEmail(){
+    const email= document.querySelector("#emailId");
+    const errorEml= document.querySelector(".errorEml")
+    const regExp= /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+    if(!regExp.test(email.value)){
+        errorEml.style.display="block";
+    } else {
+        errorEml.style.display="none";
+    }
+}
+function validatePassword(){
+    const pwd= document.querySelector("#passId");
+    const errorpwd= document.querySelector(".errorpwd")
+    const regExp= /^[A-Za-z]\w{6,}[A-Za-z]$/;
+    if(!regExp.test(pwd.value)){
+        errorpwd.style.display="block";
+    } else {
+        errorpwd.style.display="none";
     }
 }
---
+++ UI/js/reset.js
@@ -0,0 +1,12 @@
+const firstId=document.querySelector("#firstId").value;
+const secondId=document.querySelector("#secondId").value;
+const btnReset= document.querySelector("#btnId");
+btnReset.addEventListener("click", function(){
+
+    if(firstId===secondId){
+        windows.location.href="../HTML/index.html";
+    } else {
+        const errorRes= document.querySelector(".errorRes");
+        errorRes.style.display="block";
+    }
+});
