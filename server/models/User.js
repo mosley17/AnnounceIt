44ca7cd840b956ef6773e4523a0a79e22b6e2da8
mosley17 <maniraguhamosley@gmail.com>
2020-01-28 13:39:16 +0200

    creating signup api endpoint

---
+++ server/models/User.js
@@ -0,0 +1,21 @@
+const users= [];
+class User {
+    constructor(){
+        this.createUser=(body)=>{
+           const userData= {
+                "id": body.id,
+                "email": body.email,
+                "first_name": body.first_name,
+                "last_name": body.last_name,
+                "password": body.password,
+                "phone_number": body.phone_number,
+                "address": body.address,
+                "is_admin": body.is_admin
+            }
+            users.push(userData);
+            return userData;
+        }
+    }
+}
+
+export default new User();
