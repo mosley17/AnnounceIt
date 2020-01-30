9b38d02edffb7dc202bd409c89c1ad4e999fc1d7
mosley17 <maniraguhamosley@gmail.com>
2020-01-30 18:32:55 +0200

    change

--- server/models/announceModify.js
+++ server/models/announceModify.js
@@ -24,7 +24,6 @@ export const updateAnnouncement = (req, res) => {
     const announceSpec = announcements.find(announce => announce.id === parseInt(req.params.id));
     if (!announceSpec) return res.status(404).send("the given id was not found");
 
-<<<<<<< HEAD
     announceSpec.id = req.body.id;
     announceSpec.owner = req.body.owner,
         announceSpec.status = req.body.status,
@@ -36,33 +35,4 @@ export const updateAnnouncement = (req, res) => {
         "data": announceSpec
     })
 }
-export const deleteAnnouncement= (req,res) =>{
-    const announceSpec = announcements.find(announce => announce.id === parseInt(req.params.id));
-    if (!announceSpec) return res.status(404).send("the given id was not found");
-||||||| merged common ancestors
-=======
-    announceSpec.id = req.body.id;
-    announceSpec.owner = req.body.owner,
-        announceSpec.status = req.body.status,
-        announceSpec.text = req.body.text,
-        announceSpec.start_date = body.start_date,
-        announceSpec.end_date = req.body.end_date
-    res.status(200).json({
-        "status": "success",
-        "data": announceSpec
-    })
-}
->>>>>>> ft-upadate-announcement-170966918
 
-<<<<<<< HEAD
-    const trackIndex= announcements.indexOf(announceSpec);
-    announcements.splice(trackIndex,1);
-    res.status(200).json({
-        "status": "success",
-        "data": announceSpec
-    })
-}
-||||||| merged common ancestors
-export default viewAllAnnouncements;
-=======
->>>>>>> ft-upadate-announcement-170966918
